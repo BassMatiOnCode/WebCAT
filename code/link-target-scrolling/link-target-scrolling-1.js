188933752acdf1088e982a208b4d070877ebd19e
usp <75748577+BassMatiOnCode@users.noreply.github.com>
2024-11-01 10:55:05 +0100

    Ticket 2024-10-31-001 Refactoring: link-target-scrolling component added

---
+++ code/link-target-scrolling/link-target-scrolling-1.js
@@ -0,0 +1,55 @@
+// Documentation: .../web-toolbox/link-target-scrolling/link-target-scrolling.htm
+
+import * as initializer from "../component-initializer/component-initializer-1.js" ;
+
+let scrollMarginTop ;  // default scroll margin top
+
+/**
+*		scrollToElement ( )
+*		Scrolls the element into view and triggers highlighting animation
+*
+*/ export function scrollToElement( selector ) {
+	// Find the link target element
+	if ( selector?.length < 2 ) return ;  // nothing to do
+	const element = document.querySelector( selector );
+	if ( ! element ) return console.error( `Link target element not found: ${selector}` );
+	// Determine the scroll margin top
+	const event = new CustomEvent( "query-scroll-margin-top", { detail : { marginTop : null , marginBottom : null  } } ) ;
+	document.dispatchEvent( event ) ;
+	const scrollMargin = event.detail.marginTop || scrollMarginTop ;
+	element.style.scrollMarginTop =	`${ scrollMargin }px` ;
+//	element.scrollIntoView( { behavior : "smooth" , block : "start" } );
+	document.scrollingElement.scroll( { top : element.offsetTop - scrollMargin , behavior : "smooth"  } ) ;
+	function scrollEndHandler ( ) {
+		document.location.hash = selector ;
+		document.removeEventListener( "scrollend" , scrollEndHandler );
+		}
+	document.addEventListener( "scrollend" , scrollEndHandler );
+	}
+/**
+*		init ( )
+*		Initializes the WebCAT component.
+*
+*/ export function init( searchparams = new URLSearchParams( )) {
+	console.debug( "Module.init() is running" );
+	// Save and remove document fragment identifier from the URL to enable smooth scrolling.
+	const linkTargetElementSelector = document.location.hash;
+	document.location.hash = "" ;
+	// Scroll to link target element
+	scrollMarginTop = searchparams.get( "scroll-margin-top" ) || "20" ;
+	scrollToElement( linkTargetElementSelector );
+	// Add event handler to monitor anchors with links to elements on the page
+	document.addEventListener( "click" , evt => {
+		// Catch anchor clicks that navigate to an element on the same page
+		if ( evt.target.tagName !== "A" ) return ;
+//		evt.stopPropagation( );
+		evt.preventDefault( );
+		// Scroll smoothly instead.
+		scrollToElement( evt.target.hash );
+		} ) ;
+	}
+/** 
+ *		Module init code 
+ */ 
+// Initialize component
+initializer.initComponent( init, import.meta.url );
