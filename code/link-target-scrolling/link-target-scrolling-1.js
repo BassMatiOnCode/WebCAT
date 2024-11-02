7fc2728de14ccc589d968773bc0d9f255f0859a7
usp <75748577+BassMatiOnCode@users.noreply.github.com>
2024-11-02 09:56:30 +0100

    Timing issues with IFRAME hosts resoved

--- code/link-target-scrolling/link-target-scrolling-1.js
+++ code/link-target-scrolling/link-target-scrolling-1.js
@@ -9,23 +9,23 @@ let scrollMarginTop ;  // default scroll margin top
 *		Scrolls the element into view and triggers highlighting animation.
 *		Returns true if the link target element was found, or undefined otherwise.
 *
-*/ export function scrollToElement( selector ) {
+*/ export function scrollToElement( selector, restoreHash ) {
+	console.debug( "selector=", selector );
 	// Find the link target element
 	if ( selector?.length < 2 ) return ;  // nothing to do
 	const element = document.querySelector( selector );
 	if ( ! element ) return console.error( `Link target element not found: ${selector}` );
 	// Determine the scroll margin top
-	const event = new CustomEvent( "query-scroll-margin-top", { detail : { marginTop : null , marginBottom : null  } } ) ;
+	const event = new CustomEvent( "query-scroll-margins", { detail : { marginTop : null , marginBottom : null  } } ) ;
 	document.dispatchEvent( event ) ;
 	const scrollMargin = event.detail.marginTop || scrollMarginTop ;
-	element.style.scrollMarginTop =	`${ scrollMargin }px` ;
-//	element.scrollIntoView( { behavior : "smooth" , block : "start" } );
 	document.scrollingElement.scroll( { top : element.offsetTop - scrollMargin , behavior : "smooth"  } ) ;
 	function scrollEndHandler ( ) {
+		// Restore the location hash only if requested.
 		document.location.hash = selector ;
 		document.removeEventListener( "scrollend" , scrollEndHandler );
 		}
-	document.addEventListener( "scrollend" , scrollEndHandler );
+	if ( restoreHash ) document.addEventListener( "scrollend" , scrollEndHandler );
 	return true;
 	}
 /**
@@ -33,13 +33,9 @@ let scrollMarginTop ;  // default scroll margin top
 *		Initializes the WebCAT component.
 *
 */ export function init( searchparams = new URLSearchParams( )) {
-	console.debug( "Module.init() is running" );
-	// Save and remove document fragment identifier from the URL to enable smooth scrolling.
-	const linkTargetElementSelector = document.location.hash;
-	document.location.hash = "" ;
 	// Scroll to link target element
 	scrollMarginTop = searchparams.get( "scroll-margin-top" ) || "20" ;
-	scrollToElement( linkTargetElementSelector );
+	scrollToElement( linkTargetElementSelector, searchparams.get( "restore-hash" ) || false );
 	// Add event handler to monitor anchors with links to elements on the page
 	document.addEventListener( "click" , evt => {
 		// Catch anchor clicks that navigate to an element on the same page
@@ -51,5 +47,11 @@ let scrollMarginTop ;  // default scroll margin top
 /** 
  *		Module init code 
  */ 
-// Initialize component
-initializer.initComponent( init, import.meta.url );
+// Save and remove document fragment identifier from the URL to enable smooth scrolling.
+const linkTargetElementSelector = document.location.hash;
+document.location.hash = "" ;
+window.requestAnimationFrame( ( ) => { 
+window.requestAnimationFrame( ( ) => { 
+	initializer.initComponent( init, import.meta.url );
+	} );
+	} );
