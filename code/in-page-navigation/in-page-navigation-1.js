a2d8f114dc105a5cb0106597e63ec7b7dba33985
usp <75748577+BassMatiOnCode@users.noreply.github.com>
2024-11-02 19:32:23 +0100

    in-page-navigation: Support for scroll-margin-provider added

--- code/in-page-navigation/in-page-navigation-1.js
+++ code/in-page-navigation/in-page-navigation-1.js
@@ -3,7 +3,7 @@
 import * as initializer from "../component-initializer/component-initializer-1.js" ;
 
 export const settings = {
-	offsetTop : undefined ,  // compensation for toolbars and other sticky content
+	offsetTop : 0 ,  // compensation for toolbars and other sticky content
 	container : undefined ,  // reference to the container that holds the scroll targets
 	selector : undefined ,  // Defines which elements can be jumped to
 	scrollTargets : undefined  // list of heading elements in the main content container
@@ -13,6 +13,7 @@ export const settings = {
  *		scrollToNextTarget( )
  *
  */ export function scrollToNextTarget ( ) {
+	updateScrollMargin( );
 	if ( ! settings.scrollTargets ) settings.scrollTargets = document.querySelectorAll( settings.selector );
 	for ( const heading of settings.scrollTargets ) {
 		if ( heading.getBoundingClientRect( ).top > settings.offsetTop + 10  ) {
@@ -25,6 +26,7 @@ export const settings = {
  *		scrollToPreviousTarget( )
  *
  */ export function scrollToPreviousTarget ( ) {
+	updateScrollMargin( );
 	if ( ! settings.scrollTargets ) settings.scrollTargets = document.querySelectorAll( settings.selector );
 	for ( let i = settings.scrollTargets.length - 1 ; i >= 0 ; i -= 1 ) {
 		const heading = settings.scrollTargets[ i ];
@@ -53,16 +55,20 @@ export const settings = {
 	settings.scrollTargets = settings.container.querySelectorAll( settings.selector );
 	}
 /**
+ *		updateScrollMargin( )
+ *
+ */ function updateScrollMargin ( ) {
+	const event = new CustomEvent ( "query-scroll-margins" , { detail : { } } ) ; 
+	document.dispatchEvent( event );
+	settings.offsetTop = event.detail.marginTop || settings.offsetTop ;
+	}
+/**
 *		init ( )
 *		
 */ export function init( searchparams = new URLSearchParams( )) {
 	// Initialize parameters. Prefer settings members if possible.
-	settings.offsetTop = settings.offsetTop || parseInt(searchparams.get( "offsetTop" )) ;
-	if ( ! settings.offsetTop ) {
-		const element = document.querySelector( "MAIN" )?.previousElementSibling;
-		if ( element ) settings.offsetTop = element.scrollTop + element.offsetHeight ;
-		else settings.offsetTop = 0 ;
-		}
+	updateScrollMargin( );
+	settings.offsetTop = settings.offsetTop || parseInt(searchparams.get( "offsetTop" ) || 0) ;
 	settings.container = settings.container || document.getElementById( searchparams.get( "container" )) || document.querySelector( "MAIN" ) || document ;
 	settings.selector = settings.selector || searchparams.get( "selector" ) || "H1,H2,H3,H4,H5" ;
 	// (Re)Compile the list of scrollTargets
