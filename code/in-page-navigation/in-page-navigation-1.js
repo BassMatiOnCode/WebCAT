5e8d39be4c036676120ab37f5250da3aa9295f0f
usp <75748577+BassMatiOnCode@users.noreply.github.com>
2024-11-03 09:26:15 +0100

    scroll-page event interface added.

--- code/in-page-navigation/in-page-navigation-1.js
+++ code/in-page-navigation/in-page-navigation-1.js
@@ -3,7 +3,7 @@
 import * as initializer from "../component-initializer/component-initializer-1.js" ;
 
 export const settings = {
-	offsetTop : 0 ,  // compensation for toolbars and other sticky content
+	scrollMarginTop : 0 ,  // compensation for toolbars and other sticky content
 	container : undefined ,  // reference to the container that holds the scroll targets
 	selector : undefined ,  // Defines which elements can be jumped to
 	scrollTargets : undefined  // list of heading elements in the main content container
@@ -16,8 +16,8 @@ export const settings = {
 	updateScrollMargin( );
 	if ( ! settings.scrollTargets ) settings.scrollTargets = document.querySelectorAll( settings.selector );
 	for ( const heading of settings.scrollTargets ) {
-		if ( heading.getBoundingClientRect( ).top > settings.offsetTop + 10  ) {
-			document.documentElement.scroll( { top : heading.offsetTop - settings.offsetTop , behavior : "smooth"  } ) ;
+		if ( heading.getBoundingClientRect( ).top > settings.scrollMarginTop + 10  ) {
+			document.documentElement.scroll( { top : heading.offsetTop - settings.scrollMarginTop , behavior : "smooth"  } ) ;
 			return;
 			}
 		}
@@ -31,7 +31,7 @@ export const settings = {
 	for ( let i = settings.scrollTargets.length - 1 ; i >= 0 ; i -= 1 ) {
 		const heading = settings.scrollTargets[ i ];
 		if ( heading.getBoundingClientRect( ).top < 0  ) {
-			document.documentElement.scroll( { top : heading.offsetTop - settings.offsetTop , behavior : "smooth"  } ) ;
+			document.documentElement.scroll( { top : heading.offsetTop - settings.scrollMarginTop , behavior : "smooth"  } ) ;
 			return;
 			}
 		}
@@ -49,6 +49,25 @@ export const settings = {
 	window.scroll( { top : document.documentElement.scrollHeight , behavior : "smooth" } ) ;
 	}
 /**
+ *		scrollPageHandler( )
+ *
+ */ function scrollPageHandler ( evt ) {
+	switch ( evt.detail.method ) {
+	case "nextTarget" :
+		scrollToNextTarget( );
+		break;
+	case "previousTarget" :
+		scrollToPreviousTarget( );
+		break;
+	case "topOfPage" :
+		scrollToTopOfPage( );
+		break;
+	case "bottomOfPage" :
+		scrollToBottomOfPage( );
+		break;
+		}
+	}
+/**
  *		updateTargetList( )
  *
  */ export function updateTargetList( ) {
@@ -60,7 +79,7 @@ export const settings = {
  */ function updateScrollMargin ( ) {
 	const event = new CustomEvent ( "query-scroll-margins" , { detail : { } } ) ; 
 	document.dispatchEvent( event );
-	settings.offsetTop = event.detail.marginTop || settings.offsetTop ;
+	settings.scrollMarginTop = event.detail.marginTop || settings.scrollMarginTop ;
 	}
 /**
 *		init ( )
@@ -68,11 +87,15 @@ export const settings = {
 */ export function init( searchparams = new URLSearchParams( )) {
 	// Initialize parameters. Prefer settings members if possible.
 	updateScrollMargin( );
-	settings.offsetTop = settings.offsetTop || parseInt(searchparams.get( "offsetTop" ) || 0) ;
+	settings.scrollMarginTop = settings.scrollMarginTop || parseInt(searchparams.get( "scroll-margin-top" ) || 0) ;
 	settings.container = settings.container || document.getElementById( searchparams.get( "container" )) || document.querySelector( "MAIN" ) || document ;
+	// Compile the list of scroll targets
 	settings.selector = settings.selector || searchparams.get( "selector" ) || "H1,H2,H3,H4,H5" ;
-	// (Re)Compile the list of scrollTargets
-	settings.scrollTargets = settings.container.querySelectorAll( settings.selector );
+	updateTargetList( );
+	// Keep scroll targets up to date
+	if ( searchparams.has( "monitor-fragment-loading-complete" )) document.addEventListener( "fragment-loading-complete" , updateTargetList );
+	// Add event listener
+	document.addEventListener( "scroll-page" , scrollPageHandler ); 
 	}
 /** Module init code */ 
 initializer.initComponent( init, import.meta.url );
