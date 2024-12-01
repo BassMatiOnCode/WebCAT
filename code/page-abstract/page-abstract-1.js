b3f48fefb7d00818176d04e19bc1206d1f3ae5f8
usp <75748577+BassMatiOnCode@users.noreply.github.com>
2024-12-01 13:48:12 +0100

    #7-page-abstract-location Enhancement implemented. Documentation page and demo 1 created.

--- code/page-abstract/page-abstract-1.js
+++ code/page-abstract/page-abstract-1.js
@@ -1,17 +1,38 @@
 // Documentation: /webcat/page-abstract/page-abstract.htm
 
-/**
- *		init ( )
- *		Copies the meta description to the 
- *
- */ export function init ( ) {
-	if ( typeof( pageAbstract ) === "undefined" ) return ;
-	const content = document.querySelector( 'meta[name="description"]' ).getAttribute( "content" );
-	if ( ! content || content.length === 0 ) pageAbstract.remove( );
-	else pageAbstract.innerHTML = content ;
-	} ;
+import * as initializer from "../component-initializer/component-initializer-1.js" ;
+import { createElement } from "../utility/create-element/create-element.js" ;
 
-//
-// Module init code 
-const searchparams = new URL( import.meta.url ).searchParams ;
-if ( ! searchparams.has( "no-default-init" )) init( ) ;
+/**
+*		setContent ( )
+*
+*/ export function setContent( evt ) {
+	const current = evt && evt.detail.navigationInfo.current?.closest( "LI" );
+	const content = current?.querySelector( 'meta[name="abstract"]' )?.getAttribute( "content" )
+		|| document.head.querySelector( "meta[name='description']" )?.getAttribute( "content" );
+	if ( ! content ) pageAbstract.remove( );
+	else pageAbstract.innerHTML = content ;
+	}
+/**
+*		init ( )
+*		Initializes the WebCAT component.
+*
+*/ export function init( searchparams = new URLSearchParams( ), evt ) {
+	// Find or create abstract container
+	if ( typeof( pageAbstract ) === "undefined" && searchparams.get( "create-abstract" ) !== "no" && ! document.head.querySelector( "META[name='page-abstract'][content='create=no']" ) ) {
+		let reference = document.querySelector( "H1" )?.nextElementSibling ;
+		if ( ! reference ) return ;
+		const parentElement = reference.parentElement;
+		if ( reference.tagName === "HR" ) reference = reference.nextElementSibling ;
+		if ( reference.classList.contains( "subtitle" )) reference = reference.nextElementSibling ;
+		parentElement.insertBefore( createElement( "P" , { attributes : { id : "pageAbstract" } } ), reference );
+		}
+	if ( typeof( pageAbstract ) === "undefined" || pageAbstract.textContent ) return ;
+	// Find and set content
+	if ( searchparams.has( "use-description" )) setContent( null )
+	else document.addEventListener( "navigation-info-update" , setContent );
+	}
+
+// * * * Module init code * * * // 
+
+initializer.initComponent( init, import.meta.url );
