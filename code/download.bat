0eb1e9f2f26299260895afb155258ad7b45081ff
BassMati <75748577+BassMatiOnCode@users.noreply.github.com>
2025-10-18 15:52:52 +0200

    Update batch files improved

--- code/download.bat
+++ code/download.bat
@@ -1,3 +1,22 @@
-@rem update folder-name\ flags
-@rem update folder-name\file-name flags
-xcopy /d /s /b ..\..\inc\docs\webcat\%1 %1 %2
+@ECHO OFF
+SETLOCAL
+SET args=%*
+IF NOT DEFINED args GOTO :usage 
+SET filename=%~1
+CALL SET flags=%%args:*%1=%%
+SET src=\github\bassmationcode\inc\docs\webcat
+SET dst=\github\bassmationcode\webcat\code
+XCOPY /d /s /b%flags% "%src%\%1" "%dst%\%1"
+EXIT /b
+
+:usage
+ECHO Usage: download filename flags
+ECHO   XCopies files or folders with symlinks preserved.
+ECHO   src base: %src%
+ECHO   dst base: %dst%
+ECHO   Filenames are relative to src and dst base folders. 
+ECHO   Examples:
+ECHO   download pathbar\
+ECHO   download pathbar\*.*
+ECHO   download pathbar\pathbar-1.css
+EXIT /b
